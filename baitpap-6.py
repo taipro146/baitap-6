e273ed2fb092ee2f95be50495ae56651a54343b9
taipro146 <88499355+taipro146@users.noreply.github.com>
2021-08-17 13:14:52 +0700

    up to date 1

---
+++ baitpap-6.py
@@ -0,0 +1,17 @@
+a = int(input('nhập một số bất kỳ '))
+if a<0 :
+    a = a * (-1)
+    print('năm bạn nhập là ', a)
+else:
+    a = a
+    print('năm bạn nhập là ', a)
+stra =  str(a)
+if (a%4 == 0 ) and (a%100 != 0):
+    print('năm này là năm nhuận ')
+else:
+    str = str(a)
+    print('năm này không phải năm nhuận')
+quit()
+
+
+
