a7ac0a45dea8cf14e0a8e1b52db516a5b73f040b
taipro146 <bangphang000@gmail.com>
2021-08-19 15:29:46 +0700

    included %400 == 0

--- baitpap-6.py
+++ baitpap-6.py
@@ -6,7 +6,7 @@ else:
     a = a
     print('năm bạn nhập là ', a)
 stra =  str(a)
-if (a%4 == 0 ) and (a%100 != 0):
+if ((a%4 == 0 ) and (a%100 != 0)) or (a%400 == 0):
     print('năm này là năm nhuận ')
 else:
     str = str(a)
